b1bf22a9c78292d6e8ebc56783e7688ba3ee03a1
nguyentracthanh <thanhnt.b8170@st.usth.edu.vn>
2020-07-20 17:48:10 +0700

    update Dashboard

--- reactfb/src/App.js
+++ reactfb/src/App.js
@@ -4,9 +4,9 @@ import Facebook_login from './views/login/Facebook_login'
 import Page_facebook from './views/fbPages/Page_facebook'
 import SignInSide from './views/login/SignInSide'
 import Register from './views/login/Register'
-import Home from './views/dashboard/home'
+import Home from './views/dashboard/Dashboard'
 import { Route , Link, Switch, withRouter} from 'react-router-dom'
-
+import Statistic from './views/statistic/Statistic'
 
 function App() {
   const login = ({
@@ -19,6 +19,7 @@ function App() {
       <Route path= "/register" component={Register} />
       <Route path= "/home" component={Home} />
       <Route path= "/loginWithFacebook" component={Facebook_login} />
+      <Route path= "/stat" component={Statistic} />
     </Switch>
   );
 }
