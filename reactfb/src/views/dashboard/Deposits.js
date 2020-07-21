856c2479c4a70c44711a2258af08fe4632cc46e7
nguyentracthanh <thanhnt.b8170@st.usth.edu.vn>
2020-07-21 18:23:49 +0700

    login with fb

--- reactfb/src/views/dashboard/Deposits.js
+++ reactfb/src/views/dashboard/Deposits.js
@@ -1,35 +1,70 @@
-import React from 'react';
+
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
-
-function preventDefault(event) {
-  event.preventDefault();
-}
-
-const useStyles = makeStyles({
-  depositContext: {
-    flex: 1,
-  },
-});
+import FacebookLoginBtn from 'react-facebook-login';	
+import React, { useState, useEffect } from "react";
 
 export default function Deposits() {
-  const classes = useStyles();
+    const [fbData, setFbData] = useState([]);
+    const [auth, setAuth] = useState(false);
+
+    let dataItem;
+    let name= '';
+    let picture= '';
+    let user_accesstoken='';
+    let user_id='';
+    let facebookData;
+    const responseFacebook = response => {
+      console.log(response);
+      if (response.status !== 'unknown'){
+        const userInfor = response;
+            setFbData(userInfor)
+            setAuth(true)
+        
+        // name=userInfor.data.name;
+        // picture=userInfor.data.picture.data.url;
+        // user_accesstoken=userInfor.data.accessToken;
+        // user_id=userInfor.data.userID;
+        //   this.setState({
+        //       auth: true,
+        //       name: response.name,
+        //       picture: response.picture.data.url,
+        //       user_accesstoken:response.accessToken,
+        //       user_id:response.userID
+        //   });
+        }
+  }
+
+    const componentClicked = () => {
+          console.log('Facebook btn clicked');
+    }
+    
+    
+    if (auth==true){
+        facebookData = (
+            <div>
+                <Title>Profile</Title>
+                    <div>
+                        <img src={fbData.picture.data.url} alt={fbData.name}/>
+                        <br />
+                        <h2> {fbData.name}</h2>
+                    </div>
+            </div>)
+        }else{
+        facebookData = (<FacebookLoginBtn
+            appId="298749031132657"
+            autoLoad={false}
+            fields="name,picture"
+            onClick={componentClicked}
+            callback={responseFacebook} />);
+    }
+   
   return (
-    <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
-      </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
-      </Typography>
-      <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          View balance
-        </Link>
-      </div>
-    </React.Fragment>
+    <div>
+        {facebookData}
+      
+    </div>
   );
 }
